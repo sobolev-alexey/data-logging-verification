39aa36221f94d1339593ce73a2e760d2699a995d
Alexey Sobolev <sobolev@mail.de>
2021-01-23 00:11:38 +0100

    Added config

---
+++ functions/src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    firebaseUrl: 'https://us-central1-data-logging-verification.cloudfunctions.net/api',
+    defaultTag: 'DATALOGGINGVERIFICATION',
+    depth: 3, 
+    mwm: 9,
+    timeout: 10000,
+    nodes: [
+      "https://nodes.devnet.iota.org:443",
+      "https://altnodes.devnet.iota.org:443"
+    ]
+  }
+  
