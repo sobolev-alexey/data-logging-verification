5986620e2ec96db11d18dcd9a5f426164c5d9ad9
Alexey Sobolev <sobolev@mail.de>
2021-01-23 00:14:03 +0100

    Key generation

--- functions/src/helpers.js
+++ functions/src/helpers.js
@@ -1,4 +1,26 @@
 const firebase = require('firebase');
+const NodeRSA = require('node-rsa');
+
+const generateKeys = () => {
+    try {
+        const keySize = 2048;
+        const rsaKeypair = new NodeRSA({ b: keySize });
+        if (rsaKeypair.getKeySize() === keySize && 
+            rsaKeypair.getMaxMessageSize() >= Math.round(keySize / 10) &&
+            rsaKeypair.isPrivate() &&
+            rsaKeypair.isPublic()
+        ) {
+            return { 
+                publicKey: rsaKeypair.exportKey('public'), 
+                privateKey: rsaKeypair.exportKey('private')
+            };
+        } else {
+            throw new Error('Key generation failed');
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+}
 
 const verifyToken = async token => {
   const promise = new Promise((resolve, reject) => {
@@ -20,5 +42,6 @@ const verifyToken = async token => {
 };
 
 module.exports = {
+  generateKeys,
   verifyToken,
 };
