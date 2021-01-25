fa0aac5063dfba38e1cab55bc473db056bd8a59a
Alexey Sobolev <sobolev@mail.de>
2021-01-25 16:38:23 +0100

    ToDo for data controller

--- functions/src/routes/dataController.js
+++ functions/src/routes/dataController.js
@@ -14,14 +14,38 @@ exports.log = async (req, res) => {
     try {
         // Check Fields
         const params = req.body;
-        if (!params || !params.token) {
-            console.error("Verify token failed. Params: ", params);
+        if (!params || 
+            !params.payload ||
+            !params.signature ||
+            !params.streamId ||
+            !params.type 
+        ) {
+            console.error("Log data failed. Params: ", params);
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
 
-        // await verifyToken(params.token);
+        // Get user by ID from cloud database
+
+        // Verify signature 
+
+        // Verify payload type
+
+        // Get existing stream by ID + group ID
+
+        // If no stream found, create new stream
+
+        // MAM attach payload
+
+        // Store stream metadata
+
+        // Calculate and store payload hash, timestamp, message index
+
+        // Prepare response
+
+        
         return res.json({ status: "success" });
     } catch (error) {
+        console.error("Log data failed. Params: ", params, error);
         return res.send({ status: "error", error: error.message, code: error.code });
     };
 };
@@ -30,14 +54,24 @@ exports.read = async (req, res) => {
     try {
         // Check Fields
         const params = req.body;
-        if (!params || !params.token) {
-            console.error("Verify token failed. Params: ", params);
+        if (!params || !params.streamId || !params.signature) {
+            console.error("Read stream failed. Params: ", params);
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
 
-        // await verifyToken(params.token);
+        // Get user by ID from cloud database
+
+        // Verify signature 
+
+        // Get existing stream by ID + group ID
+
+        // MAM fetch payload
+
+        // Prepare response
+
         return res.json({ status: "success" });
     } catch (error) {
+        console.error("Read stream failed. Params: ", params, error);
         return res.send({ status: "error", error: error.message, code: error.code });
     };
 };
@@ -46,14 +80,36 @@ exports.verify = async (req, res) => {
     try {
         // Check Fields
         const params = req.body;
-        if (!params || !params.token) {
-            console.error("Verify token failed. Params: ", params);
+        if (!params || 
+            !params.payload ||
+            !params.signature ||
+            !params.streamId ||
+            !params.publicKey 
+        ) {
+            console.error("Verify message failed. Params: ", params);
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
 
-        // await verifyToken(params.token);
+        // Get user by ID from cloud database
+
+        // Verify signature 
+
+        // Verify payload type
+
+        // Get existing stream by ID + group ID
+
+        // MAM fetch payload
+
+        // Verify signature of fetched message with the provided public key, flag malicious
+
+        // Verify payload integrity, compare fetched message hash with stored hash
+
+        // Prepare response
+
+
         return res.json({ status: "success" });
     } catch (error) {
+        console.error("Verify message failed. Params: ", params, error);
         return res.send({ status: "error", error: error.message, code: error.code });
     };
 };
@@ -62,14 +118,53 @@ exports.trade_verify = async (req, res) => {
     try {
         // Check Fields
         const params = req.body;
-        if (!params || !params.token) {
-            console.error("Verify token failed. Params: ", params);
+        if (!params || 
+            !params.streamIdProducer   ||
+            !params.streamIdConsumer   ||
+            !params.streamIdAgreedBid  ||
+            !params.publicKeyProducer  ||
+            !params.publicKeyConsumer  || 
+            !params.publicKeyAgreedBid || 
+            !params.signature
+        ) {
+            console.error("Verify trade failed. Params: ", params);
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
 
-        // await verifyToken(params.token);
+        // Get user by ID from cloud database
+
+        // Verify signature 
+
+        // Verify payload
+
+        // Get existing producer stream by ID + group ID
+        // Get existing consumer stream by ID + group ID
+        // Get existing bid stream by ID + group ID
+
+        // MAM fetch producer stream payload
+        // MAM fetch consumer stream payload
+        // MAM fetch bid stream payload
+
+        // Verify signature of fetched producer message with the provided producer public key, flag malicious
+        // Verify signature of fetched consumer message with the provided consumer public key, flag malicious
+        // Verify signature of fetched bid message with the provided bid public key, flag malicious
+
+        // Verify producer payload integrity, compare fetched message hash with stored hash
+        // Verify consumer payload integrity, compare fetched message hash with stored hash
+        // Verify bid payload integrity, compare fetched message hash with stored hash
+
+        // Calculate produced energy
+        // Calculate consumed energy
+
+        // Verify energy amount match between producer, consumer, bid
+        
+        // Optionally verify starting time
+        
+        // Prepare response
+
         return res.json({ status: "success" });
     } catch (error) {
+        console.error("Verify trade failed. Params: ", params, error);
         return res.send({ status: "error", error: error.message, code: error.code });
     };
 };
