1a7082ab7601ba97f7865417481c0ade7122fc99
Alexey Sobolev <sobolev@mail.de>
2021-02-02 12:31:11 +0100

    Signature check refactoring

--- functions/src/auth/authorized.js
+++ functions/src/auth/authorized.js
@@ -1,17 +1,29 @@
 const { getUser } = require("../firebase");
+const { verifySignature } = require("../encryption");
 
 exports.isAuthorized = async (req, res, next) => {
     try {
         const { uid } = res.locals;
-        const { groupId } = req.body;
+        const params = req.body;
+        if (!params || !params.signature || !params.groupId || !params.payload) {
+            console.error("Log data failed. Params: ", params);
+            return res.status(400).json({ error: "Ensure all fields are included" });
+        }
 
 
         const user = await getUser(uid);
 
 
         // Verify group assignment
-        if (!user.groups.includes(groupId)) {
+        if (!user.groups.includes(params.groupId)) {
             return res.status(403).send({ status: "error", error: 'No access to given group' });
+        }
+
+        // Verify signature 
+        const signature = Buffer.from(JSON.parse(params.signature));
+        const callerSignatureVerificationResult = await verifySignature(user.publicKey, params.payload, signature);
+        if (!callerSignatureVerificationResult) {
+            return res.status(400).send({ status: "error", error: 'Wrong signature' });
         }
 
         return next();
