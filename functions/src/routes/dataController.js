17b358becdbd50ae20131617b5f549836633b22c
Alexey Sobolev <sobolev@mail.de>
2021-02-08 15:33:02 +0100

    Verification API

--- functions/src/routes/dataController.js
+++ functions/src/routes/dataController.js
@@ -1,8 +1,9 @@
 const isEmpty = require('lodash/isEmpty');
-const { isJSON, checkMessageTag, getHash } = require("../helpers");
-const { fetch, publish } = require("../streams");
+const { isJSON, checkMessageTag, getHash, getExplorerURL, fetchStream } = require("../helpers");
+const { publish } = require("../streams");
+const { verifySignature } = require("../encryption");
 const {
-    getUser,
+    getSettings,
     getStreamDetails,
     getStreamMessages,
     storeStreamMessage,
@@ -110,27 +111,13 @@ exports.read = async (req, res) => {
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
 
-        // Get existing stream by ID + group ID
-        const streamId = `${params.groupId}__${params.streamId}`;
-        const streamDetails = await getStreamDetails(streamId);
-
-        if (!streamDetails || isEmpty(streamDetails) || !streamDetails.metadata || isEmpty(streamDetails.metadata)) {
-            return res.status(400).send({ status: "error", error: 'No stream metadata found' });
+        // MAM fetch payload
+        const result = await fetchStream(params.groupId, params.streamId);
+        if (result.status !== 'success') {
+            return res.status(result.code).send({ status: result.status, error: result.error });
         }
 
-        // MAM fetch payload
-        const messages = await fetch(
-            streamDetails.metadata, 
-            params.streamId, 
-            params.groupId
-        );
-
-        // Prepare response
-        return res.json({ 
-            status: "success",
-            messages,
-            metadata: streamDetails.metadata
-        });
+        return res.json(result);
     } catch (error) {
         console.error("Read stream failed. Params: ", req.body, error);
         return res.send({ status: "error", error: error.message, code: error.code });
@@ -147,42 +134,83 @@ exports.verify = async (req, res) => {
             !params.groupId ||
             !params.publicKey 
         ) {
+
             console.error("Verify message failed. Params: ", params);
             return res.status(400).json({ error: "Ensure all fields are included" });
         }
+
+        const response = { status: "success", verified: true };
 
         // Verify payload type
         if (isEmpty(params.payload) || !isJSON(params.payload)) {
             return res.status(400).send({ status: "error", error: 'Wrong payload format' });
         }
 
-        // Get existing stream by ID + group ID
-        const streamId = `${params.groupId}__${params.streamId}`;
-        const streamDetails = await getStreamDetails(streamId);
-
-        console.log('VERIFY 3: ', JSON.stringify(streamDetails));
-
-        if (!streamDetails || isEmpty(streamDetails) || !streamDetails.metadata || isEmpty(streamDetails.metadata)) {
-            return res.status(400).send({ status: "error", error: 'No stream metadata found' });
+        // MAM fetch payload
+        const result = await fetchStream(params.groupId, params.streamId);
+        if (result.status !== 'success') {
+            response.status = 'error';
+            response.verified = false;
+            response.errorMessage = result.error;
         }
 
-        // MAM fetch payload
-        const messages = await fetch(
-            streamDetails.metadata, 
-            params.streamId, 
-            params.groupId
-        );
+        const fetchedMessages = result.messages;
+        let index = fetchedMessages.length;
+        if (params.messageIndex && Number(params.messageIndex) > 0 && Number(params.messageIndex) <= fetchedMessages.length) {
+            index = Number(params.messageIndex);
+        }
+        const fetchedMessage = fetchedMessages[index - 1];
+        if (isEmpty(fetchedMessage) || !isJSON(fetchedMessage)) {
+            response.status = 'error';
+            response.verified = false;
+            response.errorMessage = 'Message not fetched';
+        }
 
-        console.log('VERIFY 4: ', JSON.stringify(messages));
+        // Get existing stream by ID + group ID
+        const streamId = `${params.groupId}__${params.streamId}`;
+        const storedMessages = await getStreamMessages(streamId);
 
-        // Verify signature of fetched message with the provided public key, flag malicious
+        const storedMessage = storedMessages.find(msg => msg.index === index);
+        if (isEmpty(storedMessage) || !isJSON(storedMessage)) {
+            response.status = 'error';
+            response.verified = false;
+            response.errorMessage = 'Message not found';
+        }
 
         // Verify payload integrity, compare fetched message hash with stored hash
+        const payloadHash = getHash(JSON.stringify(params.payload));
+        if (payloadHash !== storedMessage.hash) {
+            // ToDo: Log malicious
+            response.status = 'error';
+            response.verified = false;
+            response.malicious = true;
+            response.errorMessage = 'Integrity error';
+        }
+
+        // Verify signature of fetched message with the provided public key, flag malicious
+        const signature = Buffer.from(JSON.parse(storedMessage.signature));
+        const signatureVerificationResult = await verifySignature(params.publicKey, fetchedMessage, signature);
+        if (!signatureVerificationResult) {
+            // ToDo: Log malicious
+            response.status = 'error';
+            response.verified = false;
+            response.malicious = true;
+            response.errorMessage = 'Wrong signature';
+        }
 
         // Prepare response
+        const settings = await getSettings();
+        const { root, sideKey } = storedMessage.metadata;
+        response.explorer = getExplorerURL(root, sideKey, settings.tangle.network);
+        
+        if (params.returnPayload) {
+            response.fetchedPayload = fetchedMessage;
+        }
+        if (params.returnMetadata) {
+            response.metadata = storedMessage.metadata;
+        }
 
-
-        return res.json({ status: "success" });
+        return res.json(response);
     } catch (error) {
         console.error("Verify message failed. Params: ", req.body, error);
         return res.send({ status: "error", error: error.message, code: error.code });
