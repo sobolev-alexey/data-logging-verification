93318ab375d7e85fb4a384cddcbc6dc30e3aa640
Alexey Sobolev <sobolev@mail.de>
2021-02-16 19:00:54 +0100

    Register script

--- cli/register.js
+++ cli/register.js
@@ -1,37 +1,40 @@
 const fs = require('fs');
+const chalk = require('chalk');
 const { generateKeys } = require('./encryption');
-const { getConfig, callApi } = require('./utils');
+const { callApi } = require('./utils');
 
-(async () => {
-    try {
-        const config = getConfig();
-        const keys = generateKeys();
-        keys.groupId = config && config.groupId;
+const register = async (email, groupId, keyFile) => {
+  try {
+    const keys = generateKeys();
+    keys.groupId = groupId;
 
+    // Store keys
+    fs.writeFileSync(keyFile, JSON.stringify(keys, undefined, "\t"));
+
+    const result = await callApi('register', {
+      email,
+      groupId,
+      publicKey: keys.publicKey
+    });
+
+    if (result && result.status === 'success') {
+      if (result.servicePublicKey) {
+        keys.servicePublicKey = result.servicePublicKey;
         // Store keys
-        fs.writeFileSync('./keys.json', JSON.stringify(keys, undefined, "\t"));
-
-        const result = await callApi('register', {
-          email: config && config.email,
-          groupId: config && config.groupId,
-          publicKey: keys.publicKey
-        });
-
-        if (result && result.status === 'success') {
-          if (result.servicePublicKey) {
-            keys.servicePublicKey = result.servicePublicKey;
-            // Store keys
-            fs.writeFileSync('./keys.json', JSON.stringify(keys, undefined, "\t"));
-          }
-          if (result.token) {
-            // Store token
-            fs.writeFileSync('./token.json', result.token);
-          }
-        } else {
-          result && result.error && console.log(result.error);
-        }
-        return result;
-    } catch (error) {
-      console.log(error)
+        fs.writeFileSync(keyFile, JSON.stringify(keys, undefined, "\t"));
+      }
+      if (result.token) {
+        // Store token
+        fs.writeFileSync('./token.json', result.token);
+      }
+      console.log(chalk.green.bold('\nRegistration successful'));
+    } else {
+      result && result.error && console.error(chalk.red.bold('\n', result.error));
     }
-})();
+    return result;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = register;
