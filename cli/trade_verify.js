da6afeb285a4ae4c595a0e4ca710ca532fe782d8
Alexey Sobolev <sobolev@mail.de>
2021-02-16 18:41:51 +0100

    Trade_verify script

--- cli/trade_verify.js
+++ cli/trade_verify.js
@@ -1,46 +1,48 @@
+const chalk = require('chalk');
 const { signMessage } = require('./encryption');
-const { getConfig, getKeys, callApi } = require('./utils');
+const { getKeys, callApi } = require('./utils');
 
-(async () => {
-    try {
-        const data = {
-          streamIdProducer: 'test-producer',
-          streamIdConsumer: 'test-consumer',
-          streamIdAgreedBid: 'test-bid'
-        };
-        
-        const config = getConfig();
-        const { groupId, streamId } = config;
+const trade_verify = async (
+  streamIdProducer, publicKeyProducer, 
+  streamIdConsumer, publicKeyConsumer, 
+  streamIdAgreedBid, publicKeyAgreedBid, 
+  groupId, returnPayload, returnMetadata, keyFile
+) => {
+  try {
+    const data = { streamIdProducer, streamIdConsumer, streamIdAgreedBid };
+    
+    // Get keys
+    const keys = getKeys(keyFile);
 
-        // Get keys
-        const keys = getKeys();
+    // Sign message
+    const signature = signMessage(keys.privateKey, data);
 
-        // Sign message
-        const signature = signMessage(keys.privateKey, data);
+    const payload = { 
+      payload: data, 
+      signature: JSON.stringify(signature),
+      streamIdProducer,
+      streamIdConsumer,
+      streamIdAgreedBid,
+      publicKeyProducer,
+      publicKeyConsumer, 
+      publicKeyAgreedBid,
+      groupId,
+      returnPayload,
+      returnMetadata
+    };
 
-        const payload = { 
-          payload: data, 
-          signature: JSON.stringify(signature),
-          streamIdProducer: 'test-producer',
-          streamIdConsumer: 'test-consumer',
-          streamIdAgreedBid: 'test-bid',
-          publicKeyProducer: keys.publicKey,
-          publicKeyConsumer: keys.publicKey, 
-          publicKeyAgreedBid: keys.publicKey,
-          groupId,
-          returnPayload: true,
-          returnMetadata: true
-        };
-
-        const result = await callApi('trade_verify', payload, true);
-        
-        if (result && !result.error && result.status === 'success') {
-          console.log(result);
-        } else {
-          result && result.error && console.log(result.error);
-        }
-        return result;
-    } catch (error) {
-      console.log(error)
+    const result = await callApi('trade_verify', payload, true);
+    
+    if (result && !result.error && result.status === 'success') {
+      console.log('\n');
+      console.log(JSON.stringify(result, null, 2));
+    } else {
+      result && result.error && console.error(chalk.red.bold('\n', result.error));
     }
-})();
+    return result;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = trade_verify;
