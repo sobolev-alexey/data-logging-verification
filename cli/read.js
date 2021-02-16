267dc7a4165316b1a4181dfce7ce0b815da2ff5d
Alexey Sobolev <sobolev@mail.de>
2021-02-16 12:28:38 +0100

    CLI read

--- cli/read.js
+++ cli/read.js
@@ -1,35 +1,34 @@
-const fs = require('fs');
+const chalk = require('chalk');
 const { signMessage } = require('./encryption');
-const { getConfig, getKeys, callApi } = require('./utils');
+const { getKeys, callApi } = require('./utils');
 
-(async () => {
-    try {
-        const config = getConfig();
-        const { groupId, streamId } = config;
+const read = async (streamId, groupId, keyFile) => {
+  try {
+    // Get keys
+    const keys = getKeys(keyFile);
 
-        // Get keys
-        const keys = getKeys();
+    // Sign message
+    const signature = signMessage(keys.privateKey, { streamId });
 
-        // Sign message
-        const signature = signMessage(keys.privateKey, { email: config.email });
+    const payload = { 
+      signature: JSON.stringify(signature),
+      payload: { streamId }, 
+      streamId,
+      groupId
+    };
 
-        const payload = { 
-          signature: JSON.stringify(signature),
-          payload: { email: config.email }, 
-          streamId,
-          groupId
-        };
-
-        const result = await callApi('read', payload, true);
-
-        if (result && !result.error && result.status === 'success') {
-          if (result.metadata) {
-            console.log(result);
-          }
-        } else {
-          result && result.error && console.log(result.status, result.error);
-        }
-    } catch (error) {
-      console.log(error)
+    const result = await callApi('read', payload, true);
+    if (result && !result.error && result.status === 'success') {
+      if (result.metadata) {
+        console.log('\n');
+        console.log(JSON.stringify(result, null, 2));
+      }
+    } else {
+      result && result.error && console.error(chalk.red.bold('\n', result.status, result.error));
     }
-})();
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = read;
