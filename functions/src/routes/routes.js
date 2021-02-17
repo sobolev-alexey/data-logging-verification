0b67b5e20fc7e1f069f672a0c39d1493bb64f08d
Alexey Sobolev <sobolev@mail.de>
2021-02-17 23:20:28 +0100

    Removed cors

--- functions/src/routes/routes.js
+++ functions/src/routes/routes.js
@@ -1,27 +1,8 @@
-const cors = require('cors');
 const { log, read, verify, trade_verify } = require('./dataController');
 
-const whitelist = [
-    'http://localhost:3000', 
-    'https://data-logging-verification.web.app', 
-    'https://data-logging-verification.firebaseapp.com'
-];
-const corsOptions = {
-    // methods: ["GET, POST, OPTIONS"],
-    // allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null);
-        } else {
-            console.error('Not allowed by CORS', origin);
-            callback(new Error('Not allowed by CORS ' + origin));
-        }
-    }
-};
-
 exports.routesConfig = app => {
-    app.post('/log', cors(corsOptions), log);
-    app.post('/read', cors(corsOptions), read);
-    app.post('/verify', cors(corsOptions), verify);
-    app.post('/trade_verify', cors(corsOptions), trade_verify);
+    app.post('/log', log);
+    app.post('/read', read);
+    app.post('/verify', verify);
+    app.post('/trade_verify', trade_verify);
 }
