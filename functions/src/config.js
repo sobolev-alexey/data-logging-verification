edb5dce3841ddde43b0028dd83922610a0007242
Alexey Sobolev <sobolev@mail.de>
2021-02-17 15:47:38 +0100

    Remove unused config

--- functions/src/config.js
+++ functions/src/config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    firebaseUrl: 'https://us-central1-data-logging-verification.cloudfunctions.net/api',
-    defaultTag: 'DATALOGGINGVERIFICATION',
-    depth: 3, 
-    mwm: 9,
-    timeout: 10000,
-    nodes: [
-      "https://nodes.devnet.iota.org:443",
-      "https://altnodes.devnet.iota.org:443"
-    ]
-  }
+  firebaseUrl: 'https://us-central1-data-logging-verification.cloudfunctions.net/api'
+}
   
